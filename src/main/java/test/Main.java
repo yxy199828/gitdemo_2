24a4abbcdc34da87ccf595ecfd3faa5c0c055a37
杨鑫源 <840613097@qq.com>
2020-07-09 11:23:34 +0800

    11 修改

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @ClassName Main
+ * @Description
+ * @Auth 杨鑫源
+ * @Date 2020/7/9 11:12
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World！ ----new update");
+    }
+}
